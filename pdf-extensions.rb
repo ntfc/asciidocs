70e70983dd222eec4638ef4d73cd166b408c13d8
Nuno Carvalho <ntfc30@gmail.com>
2015-11-07 21:33:50 +0000

    Add footer, header, fonts and table inner borders

--- pdf-extensions.rb
+++ pdf-extensions.rb
@@ -1,14 +1,35 @@
 require 'asciidoctor-pdf' unless defined? ::Asciidoctor::Pdf
 
 module AsciidoctorPdfExtensions
-  def convert_paragraph node
+  # to move a open block (--) to the bottom of the page
+  def convert_open node
     # delegate to default implementation
-    super
+    if node.role == "copyright"
+      move_node_to_bottom node
+    else
+      super
+    end
   end
 
   def layout_chapter_title node, title
-    move_down 50
+    if node.sectname == "colophon" # the colophon page
+
+    end
     super
+  end
+
+  def layout_running_content position, doc, opts = {}
+    # remove the left part from colophon footer
+    if position.to_s == "footer" && (doc.attr? 'colophon_no_left_footer')
+      puts "Removing left footer from colophon page.."
+      opts[:colophon_no_left_footer] = true
+    end
+    super
+  end
+
+  def move_node_to_bottom node
+    move_down 415
+    node.content
   end
 end
 
